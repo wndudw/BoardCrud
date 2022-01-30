96d4d1a81a7eb3bea23a41bb83207ffd386ceae6
JOO <wndudw@gmail.com>
2022-01-30 22:14:20 +0900

    list.jsp 컨트롤러 처리

---
+++ board/src/main/java/com/board/jwy/BoardController.java
@@ -0,0 +1,15 @@
+package com.board.jwy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+
+	@GetMapping("/list")
+	public void getList() {
+		
+	}
+}
